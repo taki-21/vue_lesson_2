e15ff8691bb2e734b91746bde2e4c9612081eed7
saka-21 <kasaikasai572@gmail.com>
2020-07-14 21:22:10 +0900

    first commit

---
+++ src/components/page1.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>
+      ここはページ１です。
+      あいうえお
+    </p>
+    <router-link to="/page2">Go to page2</router-link>
+    <router-link to="/page3">Go to page3</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/page3.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p class="aaa">
+      ここはページ3です。
+      かきくけこ
+    </p>
+    <router-link to="/page2">Go to page2</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.aaa{
+  color: blue;
+  font-size: large;
+  font-weight: bold;
+}
+</style>
